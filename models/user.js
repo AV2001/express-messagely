e6de609bc52ea74376d247269877a6b113b710f7
AV2001 <anirudhvadlamani2001@gmail.com>
2023-11-22 20:52:56 +0400

    implement functionality for register method

--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
 /** User class for message.ly */
 
-/** User of the site. */
+const db = require('../db');
+const bcrypt = require('bcrypt');
+const { BCRYPT_WORK_FACTOR } = require('../config');
 
+/** User of the site. */
 class User {
     /** register new user -- returns
      *    {username, password, first_name, last_name, phone}
@@ -13,7 +16,19 @@ class User {
         first_name,
         last_name,
         phone,
-    }) {}
+    }) {
+        const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+        const result = await db.query(
+            `
+            INSERT INTO users (username, password, first_name, last_name, phone, join_at)
+            VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP)
+            RETURNING username, password, first_name, last_name, phone
+            `,
+            [username, hashedPassword, first_name, last_name, phone]
+        );
+
+        return result.rows[0];
+    }
 
     /** Authenticate: is this username/password valid? Returns boolean. */
     static async authenticate(username, password) {}

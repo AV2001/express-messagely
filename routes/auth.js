48c9abb301fff8751f6e638faf541b73ca127050
AV2001 <anirudhvadlamani2001@gmail.com>
2023-11-23 07:17:07 +0400

    implement functionality for POST /register

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const ExpressError = require('../expressError');
+const { SECRET_KEY } = require('../config');
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
@@ -10,3 +17,15 @@
  *
  *  Make sure to update their last-login!
  */
+router.post('/register', async (req, res, next) => {
+    try {
+        const { username } = await User.register(req.body);
+        const token = jwt.sign({ username }, SECRET_KEY);
+        User.updateLoginTimestamp(username);
+        return res.json({ token });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+module.exports = router;

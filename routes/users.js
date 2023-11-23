e01eef2476b73b95fb917af4a27b4a8c8837bed0
AV2001 <anirudhvadlamani2001@gmail.com>
2023-11-23 11:28:12 +0400

    implement functionality for GET /:username route

--- routes/users.js
+++ routes/users.js
@@ -21,6 +21,15 @@ router.get('/', async (req, res, next) => {
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+router.get('/:username', async (req, res, next) => {
+    try {
+        const { username } = req.params;
+        const user = await User.get(username);
+        return res.json({ user });
+    } catch (err) {
+        return next(err);
+    }
+});
 
 /** GET /:username/to - get messages to user
  *
